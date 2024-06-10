4a3a9f1794734b7d09b2749a7352ff7e5352b778
Ashikur Rafi <ashikurrafi@gmail.com>
2024-06-10 17:42:29 +0600

    Added Morgan to App.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+
 const app = express();
 
-
+app.use(morgan("dev"));
 
 app.get("/test", (req, res) => {
   res.send("Test API is working [GET]");
